b76d886fe0b728daa940dc0ece7837edf9f3098f
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2025-03-09 00:18:57 -0500

    Pequeña correción al DTO de registro de grupo

--- src/main/java/co/edu/uniquindio/ingesis/restful/dtos/groups/GroupCreationRequest.java
+++ src/main/java/co/edu/uniquindio/ingesis/restful/dtos/groups/GroupCreationRequest.java
@@ -1,4 +1,12 @@
 package co.edu.uniquindio.ingesis.restful.dtos.groups;
 
-public record GroupCreationRequest() {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record GroupCreationRequest(
+        @Size(min = 3, max = 20)
+        String name,
+        @NotNull
+        String idProfessor
+) {
 }
