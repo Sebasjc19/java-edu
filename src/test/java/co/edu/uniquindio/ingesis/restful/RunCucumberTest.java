4a97eebc5ea7b4255c3b3d335961336eaa5726d0
Juandque <juanm.duqueg1@uqvirtual.edu.co>
2025-05-12 21:10:26 -0500

    Configuracion cucumber Junit5

--- src/test/java/co/edu/uniquindio/ingesis/restful/RunCucumberTest.java
+++ src/test/java/co/edu/uniquindio/ingesis/restful/RunCucumberTest.java
@@ -1,13 +1,17 @@
 package co.edu.uniquindio.ingesis.restful;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue="src/test/java/co/edu/uniquindio/ingesis/restful/steps"
-)
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "co.edu.uniquindio.ingesis.restful.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, summary")
 public class RunCucumberTest {
 }
